78489874ad16d50185532e9a89b66f2464eb8369
Jay Wick <developer@jay-wick.com>
2015-02-21 10:42:52 +1100

    Publisher events

--- ProjectPublisher/FormMain.cs
+++ ProjectPublisher/FormMain.cs
@@ -17,15 +17,62 @@ namespace Publisher
             InitializeComponent();
         }
 
+        DateTime _startTime;
+
         private void FormMain_Load(object sender, EventArgs e)
         {
-            Add("Started", 10);
+            _startTime = DateTime.Now;
+
+            var publisher = new Publisher(@"C:\Users\Jay\Dropbox\Developer\Workspace\ProjectPublisher\ProjectPublisher");
+
+            publisher.OnFailure += publisher_OnFailure;
+            publisher.OnFinishedAll += publisher_OnFinishedAll;
+            publisher.OnSuccess += publisher_OnSuccess;
+            publisher.OnUpdateProgress += publisher_OnUpdateProgress;
+
+            publisher.Start();
         }
 
-        private void Add(string message, float progress, bool success = true)
+        void publisher_OnUpdateProgress(float percentage)
         {
-            listEvents.Items.Add(message, success ? "good" : "bad").SubItems.Add(DateTime.Now.TimeOfDay.ToString());
-            progressAll.Value = (int)progress;
+            progressAll.Value = (int)percentage;
+        }
+
+        void publisher_OnSuccess(string message)
+        {
+            Post(message, Publisher.EventTypes.Success);
+        }
+
+        void publisher_OnFinishedAll(string message)
+        {
+            publisher_OnSuccess(message);
+            publisher_OnUpdateProgress(100);
+        }
+
+        void publisher_OnFailure(string message)
+        {
+            Post(message, Publisher.EventTypes.Failure);
+        }
+
+        private void Post(string message, Publisher.EventTypes result)
+        {
+            var timeInfo = (DateTime.Now - _startTime).ToString(@"mm\:ss\.fff");
+            listEvents.Items.Add(message, GetImageKeyFromEventType(result)).SubItems.Add(timeInfo);
+        }
+
+        private string GetImageKeyFromEventType(Publisher.EventTypes result)
+        {
+            switch (result)
+            {
+                case Publisher.EventTypes.Success:
+                    return "good";
+                case Publisher.EventTypes.Failure:
+                    return "bad";
+                case Publisher.EventTypes.Info:
+                    return "info";
+                default:
+                    throw new InvalidOperationException("Unexpected EventType in GetImageKeyFromEventType '" + result.ToString() + "'");
+            }
         }
     }
 }
--- ProjectPublisher/Publisher.cs
+++ ProjectPublisher/Publisher.cs
@@ -8,14 +8,80 @@ namespace Publisher
 {
     public class Publisher
     {
+        public enum EventTypes { Success, Failure, Info }
+
+        public event PublishEventHandler OnFailure;
+        public event PublishProgressEventHandler OnUpdateProgress;
+        public event PublishEventHandler OnSuccess;
+        public event PublishEventHandler OnFinishedAll;
+
+        public delegate void PublishEventHandler(string message);
+        public delegate void PublishProgressEventHandler(float percentage);
+
+        private string _path;
+        private string p;
+
+        public Publisher(string solutionPath)
+        {
+            _path = solutionPath;
+        }
+
         public void Start()
         {
-            // check if dirty
-            // rebuild RELEASE
+            if (checkRepository())
+                Success("", 10);
+            else
+                return;
+
+            if (rebuildRelease())
+                Success("", 20);
+            else
+                return;
+
             // list all tags
             // tag with next increment
             // zip
             // upload to ftp
+        }
+
+        private bool checkRepository()
+        {
+            var repo = new Repository(_path);
+
+            if (!repo.Exists)
+            {
+                Fail("Repository not found");
+                return false;
+            }
+            
+            if (!repo.IsClean)
+            {
+                Fail("Repository is dirty");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool rebuildRelease()
+        {
+            //var repo = new Builder(_path);
+            return false;
+        }
+
+        private void Fail(string message)
+        {
+            if (OnFailure != null)
+                OnFailure.Invoke(message);
+        }
+
+        public void Success(string message, float progress)
+        {
+            if (OnSuccess != null)
+                OnSuccess.Invoke(message);
+
+            if (OnUpdateProgress != null)
+                OnUpdateProgress.Invoke(progress);
         }
     }
 }
